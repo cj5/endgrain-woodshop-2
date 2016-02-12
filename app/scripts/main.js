15e562c974e1e41383ec643042476c267da176e2
cj5 <stenny.cj@hotmail.com>
2016-02-11 18:26:13 -0600

    finished js & other touches

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,48 +1,103 @@
 
+// $(document).ready(function () {
+//     var theHeight = $(window).height() - 156;
+
+//     function theHeightResize() {
+//         var theHeight = $(window).height() - 156;
+//         $('.logo-wrapper').css('height', theHeight);
+//         $(".logo-wrapper").css("height", theHeight);  
+//         console.log(theHeight);
+//     } 
+//     theHeightResize();
+//     $(window).resize(theHeightResize);   
+
+//     function stickyNav() { 
+//         $(".logo-wrapper").css('height', theHeight);
+//         if ($(window).scrollTop() > theHeight) {
+//             $('.nav').addClass('nav-fixed');
+//             $('.title').addClass('title-fixed');
+//             $('.title').stop().animate({height: "60px"},50);
+//         }
+//         if ($(window).scrollTop() < theHeight - 1) {
+//             $('.nav').removeClass('nav-fixed');
+//             $('.title').removeClass('title-fixed');
+//             $('.title').stop().animate({height: "110px"},50);
+//         } 
+//     }
+//     stickyNav();
+//     $(window).resize(stickyNav);
+//     $(window).scroll(stickyNav);
+
+//     function resizeNav() {
+//         $('a').click(function(){
+//             $('html, body').animate({
+//                 scrollTop: $( $.attr(this, 'href') ).offset().top
+//             }, 500);
+//             return false;
+//         });    
+//     }
+//     resizeNav();
+
+// });    
+
 $(document).ready(function () {
-    var theHeight = $(window).height() - 156;
+    updateContainer();
+    
+    $(window).resize(function() {
+        updateContainer();
+    });
+    
 
-    function theHeightResize() {
+    function updateContainer() {
         var theHeight = $(window).height() - 156;
-        $('.logo-wrapper').css('height', window.theHeight);  
-        console.log(theHeight);
-    } 
-    theHeightResize();
-    $(window).resize(theHeightResize);   
-
-    // $(window).resize(function() {
-    // $('.logo-wrapper').css(‘height’, window.innerHeight+’px’);
-    // });
-
-    function stickyNav() { 
-        $(".logo-wrapper").css('height', theHeight);
-        if ($(window).scrollTop() > theHeight) {
-            $('.nav').addClass('nav-fixed');
-            $('.title').addClass('title-fixed');
-            $('.title').stop().animate({height: "60px"},50);
-        }
-        if ($(window).scrollTop() < theHeight - 1) {
-            $('.nav').removeClass('nav-fixed');
-            $('.title').removeClass('title-fixed');
-            $('.title').stop().animate({height: "110px"},50);
-        } 
+        $('.logo-wrapper').css('height', theHeight);
     }
-    stickyNav();
-    $(window).resize(stickyNav);
-    $(window).scroll(stickyNav);
 
-    function resizeNav() {
+    $(window).scroll(function () {
+        var theHeight = $(window).height() - 156;
+
+    if ($(window).scrollTop() > theHeight) {
+        $('.nav').addClass('nav-fixed');
+        $('.title').addClass('title-fixed');
+        $('.title').stop().animate({height: "60px"}, 100);
+    }
+    if ($(window).scrollTop() < theHeight - 1) {
+        $('.nav').removeClass('nav-fixed');
+        $('.title').removeClass('title-fixed');
+        $('.title').stop().animate({height: "110px"}, 100);
+    }
+  });
+});
+
+    // function stickyNav() {
+    //     var theHeight = $(window).height() - 156;
+        
+    //     if ($(window).scrollTop() > theHeight) {
+    //       $('.nav').addClass('nav-fixed');
+    //       $('.nav').addClass('title-fixed');
+    //       $('.title').stop().animate({height: "60px"},50);
+    //     }
+    //     if ($(window).scrollTop() < theHeight - 1) {
+    //       $('.nav').removeClass('nav-fixed');
+    //       $('.nav').removeClass('title-fixed');
+    //       $('.title').stop().animate({height: "110px"},50);
+    //     }
+    // }    
+
+
+    // function resizeNav() {
         $('a').click(function(){
             $('html, body').animate({
                 scrollTop: $( $.attr(this, 'href') ).offset().top
             }, 500);
             return false;
         });    
-    }
-    resizeNav();
+    // }
 
-});    
 
+
+
+       
 
 
 
